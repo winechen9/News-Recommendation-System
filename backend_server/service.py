9d245b416d00a35bbbfefabc6db474e16a4d582c
winechen9 <310223526@qq.com>
2018-01-08 17:33:19 -0500

    refactor

--- backend_server/service.py
+++ backend_server/service.py
@@ -3,13 +3,14 @@ Service Backend
 """
 
 
-import json
+# import json
 import os
 import sys
+
+import operations
 from jsonrpclib.SimpleJSONRPCServer import SimpleJSONRPCServer
 
-from bson.json_util import dumps
-
+# from bson.json_util import dumps
 
 # import utils packages
 sys.path.append(os.path.join(os.path.dirname(__file__), 'utils'))
@@ -28,8 +29,7 @@ def add(num1, num2):
 def get_one_news():
     """Get one piece of news"""
     print("get_one_news is called.")
-    news = mongodb_client.get_db()['news'].find_one()
-    return json.loads(dumps(news))
+    return operations.getOneNews()
 
 
 RPC_SERVER = SimpleJSONRPCServer((SERVER_HOST, SERVER_PORT))
