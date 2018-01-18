1c231cad908a6b9bc568841effcab76f72c6bf16
winechen9 <310223526@qq.com>
2018-01-18 16:02:33 -0500

    pagination

--- backend_server/service.py
+++ backend_server/service.py
@@ -31,10 +31,14 @@ def get_one_news():
     print("get_one_news is called.")
     return operations.getOneNews()
 
+def get_news_summaries_for_user(user_id, page_num):
+    print("get_news_summaries_for_user is called with %s and %s" % (user_id, page_num))
+    return operations.getNewsSummariesForUser(user_id, page_num)
 
 RPC_SERVER = SimpleJSONRPCServer((SERVER_HOST, SERVER_PORT))
 RPC_SERVER.register_function(add, 'add')
 RPC_SERVER.register_function(get_one_news, 'getOneNews')
+PRC_SERVER.register_function(get_news_summaries_for_user, 'getNewsSummaries')
 
 print("Starting RPC server on %s:%d" % (SERVER_HOST, SERVER_PORT))
 
